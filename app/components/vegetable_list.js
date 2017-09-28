d750b9f1b47f48cd7f873e4315b10017e1856e7d
bdunams <dabadestboy1@gmail.com>
2017-09-28 16:30:14 -0400

    Vegetable List Update

--- app/components/vegetable_list.js
+++ app/components/vegetable_list.js
@@ -39,18 +39,8 @@ export default class Vegetable extends Component{
               <h4>{vegetable.VegName}</h4>
               <form method="post" onSubmit={this.handleAddToGarden}>
                 <input type="hidden" name="VegName" value={vegetable.VegName} />
-                <button type="submit" className="btn btn-success imageButton">+</button>
+                <button type="submit" className="btn btn-success"></button>
               </form>
-            <div>
-              <div className="card">
-                <img className="card-img-top" src="..." alt="Card image cap" />
-                <div className="card-body">
-                  <h4 className="card-title">{vegetable.VegName}</h4>
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" className="btn btn-primary">Go somewhere</a>
-                </div>
-              </div>
-            </div>
             </div>
           )
 
