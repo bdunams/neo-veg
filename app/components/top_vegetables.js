72fbcb38c0610f9e87630b7e9141d17dd0160b9f
bdunams <dabadestboy1@gmail.com>
2017-10-07 08:27:01 -0400

    Spelling

--- app/components/top_vegetables.js
+++ app/components/top_vegetables.js
@@ -36,7 +36,7 @@ export default class TopVegetables extends Component{
           <div className="col-md-3 col-sm-6 col-xs-12 cardSpacing">
               <div className="service-item fourth-service">
                   <div className='floated_img'><img className="homeImages" src="images/Radishes.jpg"/></div>
-                  <p><strong>Sign-up now for the fulle NEOVEG experience.</strong></p>
+                  <p><strong>Sign-up now for the full NEOVEG experience.</strong></p>
               </div>
           </div>           
         </div>
